906fa22a3532205cc4144b8896e0cd84024c44db
bestoa <bestoapache@gmail.com>
2017-12-18 12:36:28 +0800

    Refine private demo.

--- priv_demo/nanopool2.py
+++ priv_demo/nanopool2.py
@@ -13,36 +13,44 @@ if len(sys.argv) < 2:
     exit()
 
 pp = PeriodicPusher(sys.argv[1])
-is_offline = False
+last_hashrate = 0
 
-@pp.notification_register
-def offline_checker(config):
-    global is_offline
-    print('Offline check...')
+def get_report_hashrate(config):
     try:
         r = requests.get(config['API_BASE'] + config['ACCOUNT'])
         if r.status_code != 200:
             print('Request failed, status: ' + r.status_code)
-            return (None, False);
+            return (False, 0)
         result = json.loads(r.text)
         if not result['status']:
             print('Api call failed, error:  ' + result['error'])
-            return (None, False);
-        if result['data'] < config['THRESHOLD']:
-            if not is_offline:
-                is_offline = True
-                return (result['data'], True);
-            else:
-                print('Offline, but message has already been pushed. Skip...')
-                return (None, False);
-        else:
-            if is_offline:
-                is_offline = False
-            print('Online!')
-            return (None, False);
+            return (False, 0)
+        return (True, result['data'])
     except:
         print('Exception happened.')
-        return (None, False);
+        return (False, 0)
+
+@pp.notification_register
+def offline_checker(config):
+    global last_hashrate
+    print('Offline check...')
+    ret, current_hashrate = get_report_hashrate(config)
+    if ret:
+        print('Current hashrate is %.2f' % current_hashrate)
+        high = last_hashrate * (100 + config['THRESHOLD']) / 100
+        low = last_hashrate * (100 - config['THRESHOLD']) / 100
+        last_hashrate = current_hashrate
+        if current_hashrate > high or current_hashrate < low:
+            return (current_hashrate, True)
+    return (None, False);
+
+@pp.prepare
+def init(config):
+    global last_hashrate
+    ret, last_hashrate = get_report_hashrate(config)
+    if not ret:
+        raise Exception('Init hashrate failed.')
+    print('Init hashrate is %.2f' % last_hashrate)
 
 if __name__ == '__main__':
     pp.run()
