32d4194b4e440a704924f8e5e846868740cf3952
Besto Apache <bestoapache@gmail.com>
2018-01-02 17:25:03 +0800

    Refine test code

--- tests/test_http.py
+++ tests/test_http.py
@@ -1,16 +1,34 @@
-import sys
+import http.server
+import socketserver
+import threading
 from PeriodicPusher.Utils import HttpHelper
 
-def test_http_200():
-    r = HttpHelper.get('http://127.0.0.1:8000')
-    assert r.text == 'Hello\n'
+httpd = socketserver.TCPServer(('', 8008), http.server.SimpleHTTPRequestHandler)
+server = threading.Thread(target=httpd.serve_forever)
 
-def test_http_404():
-    r = HttpHelper.get('http://127.0.0.1:8000/invalid')
-    assert r == None 
+class TestHttp:
+    @classmethod
+    def setup_class(cls):
+        server.daemon = True
+        print('Start http server...')
+        server.start()
 
-def test_http_err_check():
-    r = HttpHelper.get('http://127.0.0.1:8000', err_check=lambda x:True)
-    assert r == None 
-    r = HttpHelper.get('http://127.0.0.1:8000', err_check=lambda x:False)
-    assert r.text == 'Hello\n'
+    @classmethod
+    def teardown_class(cls):
+        print('Stop http server...')
+        httpd.shutdown()
+
+    def test_http_200(self):
+        r = HttpHelper.get('http://127.0.0.1:8008/tests/index.html')
+        assert r.text == 'Hello\n'
+
+    def test_http_404(self):
+        r = HttpHelper.get('http://127.0.0.1:8008/invalid')
+        assert r == None
+
+    def test_http_err_check(self):
+        r = HttpHelper.get('http://127.0.0.1:8008/tests/index.html', err_check=lambda x:True)
+        assert r == None
+        r = HttpHelper.get('http://127.0.0.1:8008/tests/index.html', err_check=lambda x:False)
+        assert r.text == 'Hello\n'
+
